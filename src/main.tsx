6cfa6dbf490deee0bcaf9302ba309a658cf49603
mdolidehasan <olidehasan444@gmail.com>
2024-09-17 11:05:17 +0600

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Router.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
