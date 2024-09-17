38e6501fb89731e71af14e0f12ed9a7a833caf39
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-09-17 14:30:58 +0600

    added: login and Registration Page

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../Pages/Authentication/Login/Login";
+import RegisterPage from "../Pages/Authentication/Registration/Registration";
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <div>Hello world!</div>,
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage/>,
     },
   ]);
